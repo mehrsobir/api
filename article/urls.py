5e3e077e79d7125b2cf7cbc72b1fd8e3644cc278
Mehr <mehrsobir2@gmail.com>
2021-09-04 13:59:43 +0300

    add urls

--- article/urls.py
+++ article/urls.py
@@ -2,12 +2,8 @@
 # from .views import ArtListView, search
 # from rest_framework import routers
 #
-# router = routers.DefaultRouter()
-# router.register(r'articles', ArtListView, 'article')
-#
 #
 # urlpatterns = [
-#     path('', router.urls, name='articles'),
 #     # path('article/<int:pk>', ArtDetailView.as_view(), name='art-detail'),
 #     # path('category/<int:id>', ArtCView.as_view(), name='category'),
 #     # path('type/<int:id>', ArtTView.as_view(), name='type'),
