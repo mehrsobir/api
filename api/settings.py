8e42b09a4b1b4e27ed770e8e4adf7405bc2b8461
Mehr <mehrsobir2@gmail.com>
2021-09-08 10:29:32 +0300

    added search & odering functionalities

--- api/settings.py
+++ api/settings.py
@@ -148,5 +148,5 @@ LOGIN_REDIRECT_URL = '/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 1
 }
--- article/views.py
+++ article/views.py
@@ -1,11 +1,14 @@
 from .models import Article, Category, Type
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import ArtSerialiser, CatSerialiser, TypeSerialiser
 
 
 class ArtView(viewsets.ModelViewSet):
     serializer_class = ArtSerialiser
     queryset = Article.objects.all()
+    filter_backends = (SearchFilter, OrderingFilter,)
+    search_fields = ('title', 'text', 'author__username')
 
 class CatView(viewsets.ModelViewSet):
     serializer_class = CatSerialiser
