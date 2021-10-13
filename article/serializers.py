86d400f985a371a01b89e34178488f45431c51a2
Mehr <mehrsobir2@gmail.com>
2021-10-13 17:30:17 +0300

    article serializer

--- article/serializers.py
+++ article/serializers.py
@@ -4,24 +4,24 @@ from django.contrib.auth.models import User
 
 
 class ArtSerialiser(serializers.ModelSerializer):
-    # type = serializers.SerializerMethodField('get_type')
+    typeS = serializers.SerializerMethodField('get_type')
     # author = serializers.SerializerMethodField('get_author')
-    # category = serializers.SerializerMethodField('get_category')
+    categoryS = serializers.SerializerMethodField('get_category')
 
     class Meta:
         model = Article
-        fields = ('id', 'author', 'title', 'slug', 'annotation', 'text', 'source', 'type', 'category',
-                  'pubdate', 'updated', 'views',)
+        fields = ('id', 'author', 'title', 'slug', 'annotation', 'text', 'source', 'type', 'typeS', 'category',
+                  'categoryS', 'pubdate', 'updated', 'views',)
 
     # def get_author(self, article):
     #     username = article.author.user_name
     #     return username
-
-    # def get_type(self, article):
-    #     type = article.type.type
-    #     return type
     #
-    # def get_category(self, article):
-    #     type = article.category.category
-    #     return type
+    def get_type(self, article):
+        type = article.type.type
+        return type
+    #
+    def get_category(self, article):
+        type = article.category.category
+        return type
 
